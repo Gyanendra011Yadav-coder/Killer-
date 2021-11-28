94df26d909763864e6dfdcf2140d699530cc06ff
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-28 22:50:54 +0530

    Created The LogIn Activity To Create The Log In Page Of The app.

---
+++ app/src/main/java/com/example/killer/LogInPage.java
@@ -0,0 +1,14 @@
+package com.example.killer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogInPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_page);
+    }
+}
