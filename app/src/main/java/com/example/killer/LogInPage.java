6121c965b3cd8a2c0e38b41957415cd740b12168
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-12-10 07:45:31 +0530

    Done more Opertaions 1)changed the UI of The Log In Page 2)Added the Back-End wrodk of LogIn Page. 3) Connected the App with the Firebase.

--- app/src/main/java/com/example/killer/LogInPage.java
+++ app/src/main/java/com/example/killer/LogInPage.java
@@ -2,13 +2,56 @@ package com.example.killer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class LogInPage extends AppCompatActivity {
-
+    TextView forgetPassword, signUp;
+    EditText userName,password;
+    Button logIn;
+    FloatingActionButton googleBtn,facebookBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_page);
+
+        //Assigning Values To The Initialised Variables
+        forgetPassword=findViewById(R.id.forgotPasswordButton);
+        signUp =findViewById(R.id.signUpTextView);
+        userName=findViewById(R.id.inputUserName);
+        password=findViewById(R.id.password);
+        logIn=findViewById(R.id.logInButton);
+        googleBtn=findViewById(R.id.fab_google);
+        facebookBtn=findViewById(R.id.fab_fb);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, pass;
+                email = userName.getText().toString();
+                pass = password.getText().toString();
+
+
+            }
+        });
+
+
+
+        //This Code Is for Changing The Activity for Sign-Up.
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LogInPage.this, "Moving To SignUp", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LogInPage.this, SignUpActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/killer/MainActivity.java
+++ app/src/main/java/com/example/killer/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.killer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //this is the code of changing The activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, LogInPage.class));
+            }
+        },2000);
+
     }
 }
